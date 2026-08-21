c149c1ed494c173daae9e54fa36c07fdb32343d9
cristian-rodriguez14 <cristian.rodriguez9011@gmail.com>
2026-08-21 12:29:04 -0500

    Se modifico la configuración para que pueda ejecutarse el proyecto de forma local más facilmente

--- config/asgi.py
+++ config/asgi.py
@@ -2,21 +2,20 @@
 ASGI config for rosillo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from apps.common.middleware_ws import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 
+from django.core.asgi import get_asgi_application
+
 django_asgi_app = get_asgi_application()
 
-from apps.common.routing import websocket_urlpatterns  # noqa: E402
+from channels.routing import ProtocolTypeRouter, URLRouter
+from apps.common.middleware_ws import JWTAuthMiddleware
+from apps.common.routing import websocket_urlpatterns
+
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
--- config/settings/base.py
+++ config/settings/base.py
@@ -93,6 +93,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
+ASGI_APPLICATION = 'config.asgi.application'
 
 
 # Database
